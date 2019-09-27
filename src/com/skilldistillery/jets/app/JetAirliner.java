5f73892b446ab6106afef16ed4741d92b8f465fe
Jerry Rogers <jerryrogersjr@gmail.com>
2019-09-27 15:09:59 -0600

    classes

---
+++ src/com/skilldistillery/jets/app/JetAirliner.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.jets.app;
+
+public class JetAirliner extends Jet {
+
+	public JetAirliner(String model, double speed, int range, long price) {
+		super(model, speed, range, price);
+		
+		
+	}
+	
+
+}
